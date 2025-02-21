f2ba8f6e9b4b2175468e21f3d26336c8019b7209
diyaS-15 <diyaShrivastava15@gmail.com>
2025-02-21 05:04:33 -0700

    Added components folder & .tsx files for each section
    - added components folder and following files inside it: navbar, footer, homepage, aboutus
    - changes background and foreground colors
    - started basic navbar functionality
    - removed next.js infromation from homepage and converted to basic homepage

---
+++ website/app/components/AboutUs.tsx
@@ -0,0 +1,7 @@
+// this is where the about section will be implemented
+
+export default function AboutUs() {
+    return (
+        <h1>this is the about us page</h1>
+    );
+  }
---
+++ website/app/components/Footer.tsx
@@ -0,0 +1,7 @@
+// this is where the footer will be implemented 
+
+export default function Footer() {
+    return (
+        <h1>this is the footer</h1>
+    );
+  }
