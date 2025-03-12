130403cfa74b0018f6a45ed7ae4ae1c01bc5c788
ms-sparkle <keefefoster@gmail.com>
2025-03-11 21:37:31 -0700

    Updated image and text on homepage as well as added styling

--- website/app/components/Homepage.tsx
+++ website/app/components/Homepage.tsx
@@ -1,9 +1,37 @@
-// this is where the homepage will be implemented
+import Image from 'next/image';
 
 export default function Homepage() {
     return (
-        <section id="home"className="h-screen">
-            <h1>this is the homepage</h1>
-        </section>
+        <main>
+            {/* First Section - Full Screen Background */}
+            <section 
+                id="home" 
+                className="h-screen bg-[url('/img/front-image.jpg')] bg-no-repeat bg-center bg-cover flex items-center justify-center text-white"
+            >
+                <h1 className="text-4xl font-bold text-center">A Safer World, Powered By Paragon</h1>
+            </section>
+
+            {/* Second Section - Scroll Down with Image on the Left */}
+            <section className="h-screen flex items-center px-10">
+                <div className="w-1/2 flex justify-center">
+                    <div className="relative h-48 w-48 md:h-96 md:w-96">
+                        <Image
+                            src="/img/front-page-fire.jpg"
+                            alt="Front page fire"
+                            layout="fill"
+                            objectFit="cover"
+                            className="rounded drop-shadow-[0_0_20px_rgba(191,110,0,0.4)]"
+
+                        />
+                    </div>
+                </div>
+                <div className="w-1/2">
+                    <h2 className="text-3xl font-semibold text-center">Wildfires in the U.S. destroyed over 8 MILLION acres of land in 2024, costing billions in damages</h2>
+                    <p className="mt-4 text-lg text-white text-center">
+                        We leverage cutting-edge drone technology prevent wildfires before they start.
+                    </p>
+                </div>
+            </section>
+        </main>
     );
-  }
+}
